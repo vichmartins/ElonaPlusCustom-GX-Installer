ddaf0e23ed817f124fb3df06bc6393fe7ae8117a
Victor <vich.martins@gmail.com>
2022-09-11 03:49:46 -0400

    added new link

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.1",
     author="Gpx45",
     packages=["elonadl"],
-    description="ElonaPlusGXDownload",
+    description="ElonaPlusCGXDownload",
     long_description="Quick script to download and setup Elona+CGX",
     python_requires='>=3.10',
     install_requires=['py7zr']

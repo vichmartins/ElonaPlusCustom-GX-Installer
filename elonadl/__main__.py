72e9d581a7cebd17d905451519663a43e296ed2f
Victor <vich.martins@gmail.com>
2022-09-11 03:40:43 -0400

    added code

---
+++ elonadl/__main__.py
@@ -0,0 +1,31 @@
+from .Download import *
+from .Extract import *
+
+if __name__ == '__main__':
+
+    GH_API_ELONA = "https://api.github.com/repos/Gpx45/ElonaPlusGXDownload/releases/latest"
+    GH_API_CGX = "https://api.github.com/repos/Ruin0x11/ElonaPlusCustom-GX/releases/latest"
+    ELONA_FILE_NAME = 'elona.zip'
+    CGX_FILE_NAME = 'cgx.7z'
+
+    def main():
+        print("downloading latest version of Elona+ to your %APPDATA% folder")
+        elona = Download(file_name=ELONA_FILE_NAME, link=GH_API_ELONA)
+        elona.create_file()
+        elona.get_file()
+
+        print("downloading latest version of CustomGX variant.")
+        cgx = Download(file_name=CGX_FILE_NAME, link=GH_API_CGX)
+        cgx.create_file()
+        cgx.get_file()
+
+        print("extracting Elona+")
+        e = Extract(elona.get_name())
+        e.extract_zip()
+
+        print("setting up CustomGX")
+        c = Extract(cgx.get_name())
+        c.extract_7z()
+        print("DONE!")
+
+    main()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="elonadl",
+    version="0.0.1",
+    author="Gpx45",
+    packages=["elonadl"],
+    description="ElonaPlusGXDownload",
+    long_description="Quick script to download and setup Elona+CGX",
+    python_requires='>=3.10',
+    install_requires=['py7zr']
+)

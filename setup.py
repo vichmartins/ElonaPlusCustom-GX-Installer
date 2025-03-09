ac77b5e760edf96bfe131671db4eb22098b38feb
Victor Martins <vich.martins@gmail.com>
2025-03-09 03:23:22 -0400

    v0.1.10 Fix bug

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="elonadl",
-    version="0.1.9",
+    version="0.1.10",
     author="vichmartins",
     packages=["elonadl"],
     description="ElonaPlusCGXDownload",
-    long_description="Installs the latest version of Elona+CGX (Windows Only Currently)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     python_requires='>=3.10',
     install_requires=['pywin32', 'requests', 'winshell']
 )

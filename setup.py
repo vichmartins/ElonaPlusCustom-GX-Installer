36462b80576b4396f51e03126d6db7c9d3f4df78
Victor Martins <vich.martins@gmail.com>
2025-03-08 23:09:27 -0500

    v.01.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="elonadl",
-    version="0.1.7",
+    version="0.1.8",
     author="vichmartins",
     packages=["elonadl"],
     description="ElonaPlusCGXDownload",
